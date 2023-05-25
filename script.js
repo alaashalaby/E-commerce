2dc73186fd6e287c97af3c439e019c1c7b4aa448
Alaa Shalaby <80048047+alaashalaby@users.noreply.github.com>
2023-05-25 14:07:52 +0200

    Commit

--- script.js
+++ script.js
@@ -38,78 +38,163 @@ urlIdParameter=urlparams.get('id');
 console.log(urlIdParameter);
 const homeProducts=[
     {
-    image:'./Images/img_1.jpg',
+        title:"T shirt 100% Cotton",
+        price:85,
+        Available:5,
+        image:'./Images/img_1.jpg',
     },
     {
-    image:'./Images/img_6.jpg',
+        title:"T shirt 100% Cotton",
+        price:95,
+        Available:9,
+        image:'./Images/img_6.jpg',
     },
     {
+        title:"T shirt 100% Cotton",
+        price:100,
+        Available:7,
         image:'./Images/img_7.jpg',
     },
     {
-    image:'./Images/img_9.jpg',
+        title:"T shirt 100% Cotton",
+        price:150,
+        Available:3,
+        image:'./Images/img_9.jpg',
     },
     {
-    image:'./Images/img_13.jpg',
+        title:"High Waist Trouser",
+        price:110,
+        Available:3,
+        image:'./Images/img_13.jpg',
     },
     {
-    image:'./Images/img_10.jpg',
+        title:"Skirts Women",
+        price:150,
+        Available:14,
+        image:'./Images/img_10.jpg',
     },
     {
-    image:'./Images/img_5.jpg',
+        title:"T shirt 100% Cotton",
+        price:120,
+        Available:3,
+        image:'./Images/img_5.jpg',
     },
     {
-    image:'./Images/img_18.jpg',
+        title:"T shirt 100% Cotton",
+        price:130,
+        Available:9,
+        image:'./Images/img_18.jpg',
     },
     {
-    image:'./Images/img_14.jpg',
+        title:"T shirt 100% Cotton",
+        price:110,
+        Available:3,
+        image:'./Images/img_14.jpg',
     },
     {
-    image:'./Images/img_20.jpg',
+        title:"Crochet Scarf",
+        price:150,
+        Available:13,
+        image:'./Images/img_20.jpg',
     },
     {
-    image:'./Images/img_19.jpg',
+        title:"High Waist Trouser",
+        price:120,
+        Available:15,
+        image:'./Images/img_19.jpg',
     },
     {
-    image:'./Images/img_21.jpg',
+        title:"Crochet Scarf",
+        price:130,
+        Available:3,
+        image:'./Images/img_21.jpg',
     },
     {
-    image:'./Images/img_28.jpg',
+        title:"Rolex Watches",
+        price:85,
+        image:'./Images/img_28.jpg',
     },
     {
-    image:'./Images/img_30.jpg',
+        title:"Rolex Watches",
+        price:95,
+        Available:4,
+        image:'./Images/img_30.jpg',
     },
     {
-    image:'./Images/img_25.jpg',
+        title:"Rolex Watches",
+        price:100,
+        Available:10,
+        image:'./Images/img_25.jpg',
     },
     {
-    image:'./Images/img_26.jpg',
+        title:"Rolex Watches",
+        price:150,
+        Available:2,
+        image:'./Images/img_26.jpg',
     },
     {
-    image:'./Images/img_22.jpg',
+        title:"Heart Bracelet",
+        price:35,
+        Available:11,
+        image:'./Images/img_22.jpg',
     },
     {
-    image:'./Images/img_29.jpg',
+        title:"Summer Bracelet",
+        price:25,
+        image:'./Images/img_29.jpg',
     },
     {
-    image:'./Images/img_31.jpg',
+        title:"Gold Ring",
+        price:45,
+        image:'./Images/img_31.jpg',
     },
     {
-    image:'./Images/img_32.jpg',
+        title:"Gold Ring",
+        price:15,
+        image:'./Images/img_32.jpg',
     },
     {
-    image:'./Images/img_33.jpg',
+        title:"Baseball Caps",
+        price:15,
+        image:'./Images/img_33.jpg',
     },
     {
-    image:'./Images/img_34.jpg',
-    },
-    {
-    image:'./Images/img_35.jpg',
-    },
-    {
-    image:'./Images/img_36.jpg',
+        title:"Baseball Caps",
+        price:30,
+        image:'./Images/img_34.jpg',
+},
+{
+        title:"Baseball Caps",
+        price:20,
+        image:'./Images/img_35.jpg',
+},
+{
+        title:"Baseball Caps",
+        price:50,
+        image:'./Images/img_36.jpg',
     }
 ]
 let productImage = document.getElementById("item");
-console.log(productImage);
+let productTitle = document.getElementById("product-title");
+let productPrice = document.getElementById("product-price");
+let productAvailable = document.getElementById("product-availbilty");
+
 productImage.src=homeProducts[urlIdParameter].image;
+productPrice.textContent=`$ ${homeProducts[urlIdParameter].price}`;
+productTitle.textContent=homeProducts[urlIdParameter].title;
+productAvailable.textContent=`Avalibility & Stock : ${homeProducts[urlIdParameter].Available}`;
+
+/* =============================================================== */
+let itemSize = document.querySelectorAll(".item-size");
+itemSize.forEach((item) => {
+    item.addEventListener("click" , ()=>{
+        removeActive();
+        item.classList.add("active");
+    })
+})
+
+let removeActive = ()=>{
+    itemSize.forEach((item) => {
+        item.classList.remove("active");
+    })
+}
